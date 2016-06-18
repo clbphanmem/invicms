4b8da49b2762d29e33cf03bccb9fdc5825e0f6e4
Andy Vu <clbphanmem@gmail.com>
2016-06-18 23:22:06 +0700

    Update Hook Action

--- Cores/Hook.php
+++ Cores/Hook.php
@@ -6,7 +6,11 @@ class Hook
     public static $actionArr = [];
     protected static $filterArr = [];
 
-    public static function getInstance()
+    /**
+     * Get instance of Hook
+     * @return Hook
+     */
+    public static function getInstance(): Hook
     {
         if (static::$instance === null) {
             static::$instance = new static();
@@ -15,39 +19,60 @@ class Hook
     }
 
     /**
-     * TODO something at defined position
+     * Do something at defined position
      * @param string $position
+     * @param bool $lowestToHighest
      * @return Hook
      */
     public static function doAction(string $position, bool $lowestToHighest = true): Hook
     {
-        $hooks = static::sortPriority(static::$actionArr[$position], $lowestToHighest);
-        foreach ($hooks as $hook) {
-            call_user_func($hook['callback']);
+        if (isset(static::$actionArr[$position])) {
+            $hooks = static::sortPriority(static::$actionArr[$position], $lowestToHighest);
+            foreach ($hooks as $hook) {
+                call_user_func_array($hook['callback'], $hook['vars']);
+            }
         }
         return static::getInstance();
     }
 
+    /**
+     * Apply some style
+     * @param string $filterName
+     * @param $callback
+     * @param bool $runTopToBottom
+     * @return Hook
+     */
     public static function applyFilters(string $filterName, $callback, bool $runTopToBottom = true): Hook
     {
         return static::getInstance();
     }
 
     /**
-     * Do something
-     * @return mixed
+     * Add action to array
+     * @param string $actionName
+     * @param $callback
+     * @param int $priority
+     * @param array ...$vars
+     * @return Hook
      */
-    public static function action(string $actionName, $callback, int $priority = 10): Hook
+    public static function action(string $actionName, $callback, int $priority = 10, ...$vars): Hook
     {
         if ($actionName && !empty($callback)) {
             static::$actionArr[$actionName][] = [
                 'priority' => $priority,
-                'callback' => $callback
+                'callback' => $callback,
+                'vars' => $vars
             ];
         }
         return static::getInstance();
     }
 
+    /**
+     * Sort Priority of Array
+     * @param array $arr
+     * @param bool $lowestToHighest
+     * @return array
+     */
     public static function sortPriority(array $arr, bool $lowestToHighest = true): array
     {
         foreach ($arr as $k1 => $v1) {
@@ -74,5 +99,15 @@ class Hook
     public static function filter(string $filterName, $callback): Hook
     {
         return static::getInstance();
+    }
+
+    /**
+     * Catch error when Hook used in String
+     * @return string
+     */
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+        return '';
     }
 }
--- index.php
+++ index.php
@@ -5,55 +5,45 @@ use Cores\Database as Database;
 use Cores\Hook as Hook;
 use Cores\Models\Category as Category;
 
-Route::get('/', function () { // Lỗi khi bỏ "get" vẫn chạy
-//    echo Category::name();
 
-    Hook::action('header', function () {
-        echo 'One2<br/>';
-    });
-
-    Hook::action('header', function () {
-        echo 'Two5<br/>';
-    }, 7);
-
-    Hook::action('header', function () {
-        echo 'Two3<br/>';
-    }, 7);
-
-    Hook::action('header', function () {
-        echo 'Three<br/>';
-    }, 9);
-
-    Hook::action('header', function () {
-        echo 'One1<br/>';
-    });
-
-
-//    echo dump(Hook::sortPriority(Hook::$actionArr['header']));
-    echo '<p>------</p>';
-    Hook::doAction('header');
-    echo '<p>------</p>';
+Hook::action('header', function () {
+    echo 'This is header';
 });
 
-Route::get('abc/{id}/more', function($id) { // Lỗi khi bỏ "get" vẫn chạy
-    echo 'More ' . $id;
+
+// Support to use Closure and environment variables or any additional arguments
+$ev = 'This is Environment Variable';
+Hook::action('body', function ($any, $eny) use ($ev) {
+    echo '<p>This is header</p>';
+    echo "<p>EV: $ev</p>";
+    echo "<p>$any</p>";
+    echo "<pre>" . print_r($eny, true) . "</pre>";
+}, 10, 'This is additional arguments', [1, 2, 3]);
+
+// Support Low/High Priority (Default priority is 10 likes WordPress)
+Hook::action('body', function () {
+    echo '<p>Three</p>';
+}, 9);
+
+Hook::action('body', function () {
+    echo '<p>Two</p>';
+}, 16);
+
+Hook::action('body', function () {
+    echo '<p>One</p>';
+}, 5);
+
+Hook::action('footer', function () {
+    echo 'This is footer';
 });
 
-Route::get('baby/{id}', 'TestController@index');
-
-//Route::get('/', function() {
-//    return 'OK Lar';
-//});
-
-//$route->get('abc/{id}/more', function($id) {
-//    echo 'Baba World ' . $id;
-//});
-//
-//
-//$route->get('bbc', 'test');
-//
-//$route->get('/', function() {
-//    echo 'Index';
-//});
+Route::get('/', function () {
+    echo '
+        <p>Demo</p>
+        <p>' . Hook::doAction('header') . '</p>
+        <div>' . Hook::doAction('body') . '</div>
+        <p>' . Hook::doAction('footer') . '</p>
+    ';
+});
 
 Route::dispatch();
